a504874348e49265d8e8bda434725eabfef986b3
user <mutukukennedy34@gmail.com>
2023-10-02 09:50:45 +0300

    models

--- app/models.py
+++ app/models.py
@@ -1,27 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy import MetaData
+from wsgiref import validate
+from datetime import datetime
 
-db = SQLAlchemy()
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates 
 
-class Hero(db.Model):
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
+class Hero(db.Model , SerializerMixin):
     __tablename__ = 'hero'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-
+    super_name = db.Column(db.String)
     power = db.relationship('Heropower', backref='hero')
 
-class Power(db.Model):
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    serialize_rules = ('-Heropower.hero',)
+class Power(db.Model, SerializerMixin):
     __tablename__ = 'power'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-
+    description = db.Column(db.String)
     hero = db.relationship('Heropower', backref='power')
 
-class Heropower(db.Model):
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    
+    serialize_rules = ('-Heropower.power',)
+class Heropower(db.Model , SerializerMixin):
     __tablename__ = 'Heropower'
 
     id = db.Column(db.Integer, primary_key=True)
-    hero_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
-    power_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    
+    serialize_rules = ('-hero.Heropower', '-power.Heropower',)
 
